642a1affc6c6f7a9551b07a20ffbe80fe170ecf3
scgordon <scgordon@hdfgroup.org>
2016-07-13 12:12:49 -0600

    dataRefinedTemplate
    Two changes, one to directory structure, so all data.csv for an
    organizations members are all in one place.
    
    Recreated the RAD for each mN using the new template with refined
    graphics.

--- scripts/Data.sh
+++ scripts/Data.sh
@@ -8,7 +8,7 @@ CrosswalkHome=$(pwd)
     java net.sf.saxon.Transform \
     -s:$CrosswalkWorkflow/collections/dummy.xml \
     -xsl:$CrosswalkHome/dialectRubrics/rubric$2.xsl \
-    -o:$ConceptMining/collections/$1/$3/$2/reports/data.csv \
+    -o:$ConceptMining/data/DataONE/$3_$2_data.csv \
     fileNamePattern=*.xml \
     recordSetPath=$CrosswalkWorkflow/collections/$1/$3/$2/xml \
     dialectName=$2 \
